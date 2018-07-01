10fd68392001dc3ed58b128fffbc370118462fb4
sandeep0814 <sandeep0814@gmail.com>
2018-07-01 10:41:42 +0530

    controllers and schemas definition

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,12 @@
 const userModel = require('../models/Users');
 const user = new userModel();
-
+//pass schemas in models query mutiple collections 
 class usersController {
     async showUsers(req, res){
         try {
 
             let usersList = await user.show(req, res);
-            res.json({
-                "response" :usersList
-            })
+            res.json({"response" :usersList})
         } catch(e){
             console.log(e);
         }
